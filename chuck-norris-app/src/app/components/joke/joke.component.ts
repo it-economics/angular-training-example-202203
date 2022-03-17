02993b5be698c7b5f7f9750699a677b7c0a2cda0
alxlchnr <alxlchnr@gmail.com>
2022-03-17 09:18:37 +0100

    Aufgabe 4

--- chuck-norris-app/src/app/components/joke/joke.component.ts
+++ chuck-norris-app/src/app/components/joke/joke.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs";
 
 @Component({
   selector: 'app-joke',
@@ -6,15 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./joke.component.scss']
 })
 export class JokeComponent implements OnInit {
-  public buttonClicked = false;
 
-  constructor() { }
+  public joke: string | undefined
+
+  constructor(private httpClient: HttpClient) {
+  }
 
   ngOnInit(): void {
   }
 
   showJoke(): void {
-    this.buttonClicked = true;
+    this.httpClient.get('http://api.icndb.com/jokes/random').pipe(
+      map(response => response as ChuckNorrisApiResponse),
+      map((response: ChuckNorrisApiResponse) => response.value.joke)
+    ).subscribe((joke: string) => this.joke = joke)
   }
 
+}
+
+interface ChuckNorrisApiResponse {
+  value: { id: number, joke: string }
 }
